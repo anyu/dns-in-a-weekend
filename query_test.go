7128f9a97aad1f6f55f41ad69e668bee83e4f09f
An Yu <1009227+anyu@users.noreply.github.com>
2023-10-26 13:47:11 -0400

    remove testify

--- query_test.go
+++ query_test.go
@@ -1,18 +1,18 @@
 package main
 
-import (
-	"testing"
+import "testing"
 
-	"github.com/stretchr/testify/assert"
-)
-
-func TestHeader(t *testing.T) {
+func TestDNSHeaderToBytes(t *testing.T) {
 	header := DNSHeader{
 		ID:            4884,
 		QuestionCount: 1,
 		Flags:         0,
 	}
+
 	expected := "\x13\x14\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00"
 	actual := string(header.toBytes())
-	assert.Equal(t, expected, actual)
+
+	if actual != expected {
+		t.Errorf("Expected: %s, but got: %s", expected, actual)
+	}
 }
